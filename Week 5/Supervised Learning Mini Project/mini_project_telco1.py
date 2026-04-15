ef382e1518359d1d6d3809250a81cd535bd32a52
Alvin Ngo <nghian242002@gmail.com>
2026-04-15 11:56:44 +0700

    Remove comments in mini_project_telco.py

--- Week 5/Supervised Learning Mini Project/mini_project_telco1.py
+++ Week 5/Supervised Learning Mini Project/mini_project_telco1.py
@@ -3,11 +3,16 @@ import pandas as pd
 import seaborn as sns
 import matplotlib.pyplot as plt
 from sklearn.datasets import fetch_california_housing
-
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LinearRegression
+from sklearn.metrics import mean_squared_error
 # Load datasets
 data = fetch_california_housing(as_frame=True)
 df = data.frame
 df.to_csv(r"D:\Python Practice\Practice\Theory and Practices\Week 5\Supervised Learning Mini Project\fetch_california_housing.csv", index=False)
+
+X = df[['MedInc', 'AveRooms', 'HouseAge', ]]
+y = df['MedHouseVal']
 
 
 # Inspect data
@@ -21,6 +26,20 @@ plt.show()
 # Check for missing values
 print("Missing values: \n", df.isnull().sum())
 
+# Split dataset
+X_train, X_test, y_train, y_test = train_test_split(
+    X, y, test_size=0.2, random_state=42)
+
+# Train Linear Regression model
+model = LinearRegression()
+model.fit(X_train, y_train)
+
+# Make predictions
+y_pred = model.predict(X_test)
+
+# Evaluate performance
+mse = mean_squared_error(y_test, y_pred)
+print("Linear Regression MSE:", mse)
 
 # Load Telco Customer Churn Dataset
 df_telco = pd.read_csv(
